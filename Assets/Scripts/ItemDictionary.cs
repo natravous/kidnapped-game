6140212685ccecb493fd9191e1373e621c017590
HiDE <msyariefhidayatulloh14@gmail.com>
2022-06-18 11:38:00 +0700

    Update ItemDictionary.cs

--- Assets/Scripts/ItemDictionary.cs
+++ Assets/Scripts/ItemDictionary.cs
@@ -4,36 +4,6 @@ using UnityEngine;
 
 public class ItemDictionary
 {
-    //static private Dictionary<string, Sprite> itemdict = new Dictionary<string, Sprite>();
-
-    //public Sprite getImage(string key)
-    //{
-    //    return itemdict[key];
-    //}
-
-    //public void addToDict(string key, Sprite img)
-    //{
-    //    if (this.isKey(key) == true)
-    //    {
-    //        return;
-    //    }
-    //    itemdict.Add(key, img);
-    //}
-
-    //public bool isKey(string key)
-    //{
-    //    return itemdict.ContainsKey(key);
-    //}
-
-    //public int totalItems()
-    //{
-    //    return itemdict.Count;
-    //}
-
-    //public Dictionary<string, Sprite> getDict()
-    //{
-    //    return itemdict;
-    //}
 
     // Item dictionary
     static private Dictionary<Key.typeKey, Sprite> keyItems = new Dictionary<Key.typeKey, Sprite>();
